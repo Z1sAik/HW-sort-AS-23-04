6087421648191baaf9afffc734bd8b8621130460
Z1sAik <ilab807@gmail.com>
2024-12-17 21:39:49 +0300

    17.12.2024 Сортирвки.release

--- main/data.h
+++ main/data.h
@@ -1,11 +1,11 @@
-#pragma once
+﻿#pragma once
 #include <vector>
 #include <random>
 #include <algorithm> 
 
-const std::vector<int> data1 = {34, 7, 23, 32, 5, 62, 32, 43, 7, 3}; // ������ ������
+const std::vector<int> data1 = {34, 7, 23, 32, 5, 62, 32, 43, 7, 3};
 
-const std::vector<double> data2 = { 3.1, 7.1, 235, 323, 5, 612, 32, 433, 7, 3 }; // ������ ������
+const std::vector<double> data2 = { 3.1, 7.1, 235, 323, 5, 612, 32, 433, 7, 3 };
 
 std::vector<int> generate_random_int_vector(size_t size, int min, int max) {
     std::vector<int> vec(size);
@@ -20,4 +20,77 @@ std::vector<int> generate_random_int_vector(size_t size, int min, int max) {
     return vec;
 }
 
-std::vector<int> random_ints = generate_random_int_vector(10, 1, 100);
+std::vector<int> generate_sorted_vector(size_t size, int min, int max) {
+    std::vector<int> vec(size);
+    for (size_t i = 0; i < size; ++i) {
+        vec[i] = min + i;
+    }
+    return vec;
+}
+
+std::vector<int> generate_reverse_sorted_vector(size_t size, int min, int max) {
+    std::vector<int> vec(size);
+    for (size_t i = 0; i < size; ++i) {
+        vec[i] = min + i;
+    }
+    std::reverse(vec.begin(), vec.end());
+    return vec;
+}
+
+std::vector<int> generate_array_with_duplicates(size_t size, int duplicate_value, int min, int max) {
+    std::vector<int> vec(size, duplicate_value);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(min, max);
+
+    for (size_t i = 0; i < size / 10; ++i) {
+        vec[i] = dis(gen);
+    }
+
+    return vec;
+}
+
+std::vector<double> generate_double_array(size_t size, double min, double max) {
+    std::vector<double> vec(size);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(min, max);
+
+    for (size_t i = 0; i < size; ++i) {
+        vec[i] = dis(gen);
+    }
+    return vec;
+}
+
+std::vector<int> generate_negative_array(size_t size, int min, int max) {
+    std::vector<int> vec(size);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(min, max);
+
+    for (size_t i = 0; i < size; ++i) {
+        vec[i] = -dis(gen);
+    }
+    return vec;
+}
+
+
+//все рандомные
+std::vector<int> random_ints100 = generate_random_int_vector(100, 1, 10000);
+std::vector<int> random_ints1000 = generate_random_int_vector(1000, 1, 10000);
+std::vector<int> random_ints10000 = generate_random_int_vector(10000, 1, 10000);
+
+//отсортированные
+std::vector<int> sorted = generate_sorted_vector(1000, 0, 999);
+
+//обратно отсортированные
+std::vector<int> reverse_sorted = generate_reverse_sorted_vector(1000, 0, 999);
+
+//большое кол-во одинаковых элементов 10% только случайные
+std::vector<int> duplicates = generate_array_with_duplicates(1000, 10, 0, 500);
+
+//Вещественные числа
+std::vector<double> random_doubles = generate_double_array(10000, -100.0, 100.0);
+
+//отриц числа
+std::vector<int> otric_numbers = generate_negative_array(10000, 1, 1000);
--- main/main.cpp
+++ main/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <chrono>
 #include <fstream>
 #include <iomanip>
@@ -13,17 +13,17 @@ template <typename T>
 void write_results_to_file(const std::string& algorithm_name, const std::vector<T>& sortedData, double duration) {
     std::ofstream outFile("results.txt", std::ios::app);
     if (outFile.is_open()) {
-        outFile << "���������� ��� ���������: " << algorithm_name << "\n";
-        outFile << "��������������� ������: ";
-        for (T num : sortedData) {
-            outFile << num << " ";
+        outFile << "Результаты для алгоритма: " << algorithm_name << "\n";
+        outFile << "Первые 10 отсортированных данных: ";
+        for (size_t i = 0; i < std::min(sortedData.size(), size_t(10)); ++i) {
+            outFile << sortedData[i] << " ";
         }
-        outFile << "\n����� ����������: " << std::fixed << std::setprecision(8) << duration << " ������\n\n";
+        outFile << "\nВремя выполнения: " << std::fixed << std::setprecision(8) << duration << " секунд\n\n";
         outFile.close();
-        std::cout << "���������� �������� � results.txt" << std::endl;
+        std::cout << "Результаты записаны в results.txt" << std::endl;
     }
     else {
-        std::cerr << "�� ������� ������� ���� results.txt" << std::endl;
+        std::cerr << "Не удалось открыть файл results.txt" << std::endl;
     }
 }
 
@@ -40,32 +40,42 @@ void Run(const std::string& method_name, Algorithm algorithm, const std::vector<
 int main() {
     setlocale(LC_ALL, "RU");
 
-    Run<int>("Kochetov_smoothsort - data1", Kochetov_TreeSort<int>, data1);
+    Run<int>("Kochetov_smoothort - data(random_ints100)", Kochetov_TreeSort<int>, random_ints100);
+    Run<int>("Kochetov_smoothort - data(random_ints1000)", Kochetov_TreeSort<int>, random_ints1000);
+    Run<int>("Kochetov_smoothort - data(random_ints10000)", Kochetov_TreeSort<int>, random_ints10000);
+    Run<int>("Kochetov_smoothort - data(sorted)", Kochetov_TreeSort<int>, sorted);
+    Run<int>("Kochetov_smoothort - data(reverse_sorted)", Kochetov_TreeSort<int>, reverse_sorted);
+    Run<int>("Kochetov_smoothort - data(duplicates)", Kochetov_TreeSort<int>, duplicates);
+    Run<double>("Kochetov_smoothort - data(random_doubles)", Kochetov_TreeSort<double>, random_doubles);
+    Run<int>("Kochetov_smoothort - data(otric_numbers)", Kochetov_TreeSort<int>, otric_numbers);
 
-    Run<double>("Kochetov_smoothsort - data2", Kochetov_TreeSort<double>, data2);
+    Run<int>("Khabarov_introspectiveSort - data(random_ints100)", Khabarov_introspectiveSort<int>, random_ints100);
+    Run<int>("Khabarov_introspectiveSort - data(random_ints1000)", Khabarov_introspectiveSort<int>, random_ints1000);
+    Run<int>("Khabarov_introspectiveSort - data(random_ints10000)", Khabarov_introspectiveSort<int>, random_ints10000);
+    Run<int>("Khabarov_introspectiveSort - data(sorted)", Khabarov_introspectiveSort<int>, sorted);
+    Run<int>("Khabarov_introspectiveSort - data(reverse_sorted)", Khabarov_introspectiveSort<int>, reverse_sorted);
+    Run<int>("Khabarov_introspectiveSort - data(duplicates)", Khabarov_introspectiveSort<int>, duplicates);
+    Run<double>("Khabarov_introspectiveSort - data(random_doubles)", Khabarov_introspectiveSort<double>, random_doubles);
+    Run<int>("Khabarov_introspectiveSort - data(otric_numbers)", Khabarov_introspectiveSort<int>, otric_numbers);
 
-    Run<int>("Kochetov_smoothort - data3", Kochetov_TreeSort<int>, random_ints);
+    Run<int>("Osharov_flashsort - data(random_ints100)", Osharov_flashsort<int>, random_ints100);
+    Run<int>("Osharov_flashsort - data(random_ints1000)", Osharov_flashsort<int>, random_ints1000);
+    Run<int>("Osharov_flashsort - data(random_ints10000)", Osharov_flashsort<int>, random_ints10000);
+    Run<int>("Osharov_flashsort - data(sorted)", Osharov_flashsort<int>, sorted);
+    Run<int>("Osharov_flashsort - data(reverse_sorted)", Osharov_flashsort<int>, reverse_sorted);
+    Run<int>("Osharov_flashsort - data(duplicates)", Osharov_flashsort<int>, duplicates);
+    Run<double>("Osharov_flashsort - data(random_doubles)", Osharov_flashsort<double>, random_doubles);
+    Run<int>("Osharov_flashsort - data(otric_numbers)", Osharov_flashsort<int>, otric_numbers);
 
-
-    Run<int>("Khabarov_introspectiveSort - data1", Khabarov_introspectiveSort<int>, data1);
-
-    Run<double>("Khabarov_introspectiveSort - data2", Khabarov_introspectiveSort<double>, data2);
-
-    Run<int>("Khabarov_introspectiveSort - data3", Khabarov_introspectiveSort<int>, random_ints);
-
-
-    Run<int>("Osharov_flashsort - data1", Osharov_flashsort<int>, data1);
-
-    Run<double>("Osharov_flashsort - data2", Osharov_flashsort<double>, data2);
-
-    Run<int>("Osharov_flashsort - data3", Osharov_flashsort<int>, random_ints);
-
-    Run<int>("Sliunchenko_dualpivotsort - data1", dualpivotsort<int>, data1);
-
-    Run<double>("Sliunchenko_dualpivotsort - data2", dualpivotsort<double>, data2);
-
-    Run<int>("Sliunchenko_dualpivotsort - data3", dualpivotsort<int>, random_ints);
-
+    Run<int>("Sliunchenko_dualpivotsort - data(random_ints100)", dualpivotsort<int>, random_ints100);
+    Run<int>("Sliunchenko_dualpivotsort - data(random_ints1000)", dualpivotsort<int>, random_ints1000);
+    Run<int>("Sliunchenko_dualpivotsort - data(random_ints10000)", dualpivotsort<int>, random_ints10000);
+    Run<int>("Sliunchenko_dualpivotsort - data(sorted)", dualpivotsort<int>, sorted);
+    Run<int>("Sliunchenko_dualpivotsort - data(reverse_sorted)", dualpivotsort<int>, reverse_sorted);
+    Run<int>("Sliunchenko_dualpivotsort - data(duplicates)", dualpivotsort<int>, duplicates);
+    Run<double>("Sliunchenko_dualpivotsort - data(random_doubles)", dualpivotsort<double>, random_doubles);
+    Run<int>("Sliunchenko_dualpivotsort - data(otric_numbers)", dualpivotsort<int>, otric_numbers);
+    
     return 0;
 }
 
